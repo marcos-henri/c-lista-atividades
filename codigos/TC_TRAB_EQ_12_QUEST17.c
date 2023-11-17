fdab26c2b99269e8dd47363ec80a904f5151abae
SkyySea <henrir1020@gmail.com>
2023-11-17 18:30:13 -0300

    correções e adições

--- codigos/TC_TRAB_EQ_12_QUEST17.c
+++ codigos/TC_TRAB_EQ_12_QUEST17.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <math.h> //biblioteca de expressões matemáticas
+#include <locale.h>
+//define os tipos que serão usados, o tipo double foi escolhido por abranger valores maiores, levando em consideração que o resultado pode ser um dízima
 
 int main() {
+     setlocale(LC_ALL, "Portuguese_Brazil.1252");
+printf("Olá, Vicente");
     int n = 9;
-    double resultado = 0.0;
+    int i= 1;
+    double somatorio = 0;
 // loop que executa o somatório
-    for (int i = 1; i <= n; i++) {
-        double termo = (pow(2, 3 * i) + pow(i, 2)) / (i + 1);
-        resultado += termo;
+    for (i = 1; i <= n; i++) {
+        double expressao = (pow(2, 3*i+ pow(i, 2))) / (i + 1);
+        somatorio += expressao;
     }
 
-    printf("O resultado da expressão é: %lf\n", resultado);
+    printf("O resultado da expressão é: %lf\n", somatorio);
+// %lf = long float = double = float com maior valor
 
     return 0;
 }
